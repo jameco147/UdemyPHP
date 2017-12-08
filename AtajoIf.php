014f56010937f934f191aa7c7971d796dc37fce8
Javi <bonjavi89@gmail.com>
2017-12-08 13:15:20 +0100

    Más ejercicios

---
+++ AtajoIf.php
@@ -0,0 +1,14 @@
+<?php
+
+$edad = (isset($edad)) ? $edad : "El usuario no ha establecido su edad" ;
+
+echo "Edad: " . $edad
+
+
+
+
+
+
+
+
+?>
---
+++ Switch.php
@@ -0,0 +1,20 @@
+<?php
+
+$mes = "Julio";
+
+
+switch ($mes) {
+  case 'Diciembre':
+    echo "Feliz Navidad";
+    break;
+
+  case 'Enero':
+    echo 'Feliz año nuevo';
+    break;
+
+    default:
+      echo 'En este mes no se celebra nada';
+      break;
+
+}
+?>
