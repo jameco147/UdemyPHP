f29c65e013053a864fff5404f859fbdd4eb00db1
Javi <bonjavi89@gmail.com>
2018-04-06 13:48:44 +0200

    Login y registro

---
+++ Practicas/login_registro/views/contenido.view.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no,
+    initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
+    <script src="https://use.fontawesome.com/b9d398e203.js"></script>
+    <link rel="stylesheet" href="css/estilos.css">
+    <title></title>
+  </head>
+  <body>
+    <div class="contenedor">
+      <h1 class="titulo">Contenido del sitio</h1>
+      <a href="cerrar.php">Cerrar sesión</a>
+      <hr class="border">
+      <div class="contenido">
+        <article>
+          <p>Hola, estás dentro del sistema, enhorabuena.
+            Puedes acceder a lo que quieras</p>
+        </article>
+      </div>
+    </div>
+
+  </body>
+</html>
